2ed73542380a284187422aee00c66ffb87cad098
kamuyi <2258858667@qq.com>
2018-09-20 16:35:50 +0800

    Test

---
+++ src/main/java/com/shsxt/test/Test.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: Test
+ * Author:   Admin
+ * Date:     2018/9/20 16:34
+ * Description: 测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+package com.shsxt.test;
+
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈测试〉
+ *
+ * @author Admin
+ * @create 2018/9/20
+ * @since 1.0.0
+ */
+public class Test {
+}
